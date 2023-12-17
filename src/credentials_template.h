2382149af82e0fc87451f3f795c8cdbd99001cdd
avi <avi>
2023-12-16 22:56:48 -0800

    update set_preferences template to be more innocuous

--- src/credentials_template.h
+++ src/credentials_template.h
@@ -4,7 +4,7 @@
 /*  template file for preloading credentials instead of reading from the file system 
  *  only applicable for [env:demo-unit-preloaded]
  *
- *  to use this file, copy this template to "credentials.h" and fill in the applicable values.  
+ *  to use this file, copy this template to "credentials.h" and uncomment/fill in the applicable values.  
  *  the list below is non-exhaustive and additional options can be added as needed.
  *  this will override anything in the file system on every startup.
 */
@@ -13,17 +13,20 @@
 void set_preferences(Preferences preferences);
 
 void set_preferences(Preferences preferences) {
-    // A non-exhaustive list of preferences for the ESP32
+    
     preferences.begin("config");
-    preferences.putString("ssid", "");
-    preferences.putString("password", "");
-    preferences.putString("det_id", "");
-    preferences.putString("api_key", "");
-    preferences.putInt("query_delay", 600);
+    
+    // A non-exhaustive list of preferences for the ESP32
+    // preferences.putString("ssid", "");
+    // preferences.putString("password", "");
+    // preferences.putString("det_id", "");
+    // preferences.putString("api_key", "");
+    // preferences.putInt("query_delay", 600);
     // preferences.putString("email", "");
     // preferences.putString("emailEndpoint", "");
     // preferences.putString("emailKey", "");
     // preferences.putString("emailHost", "");
+    
     preferences.end();
 }
 
